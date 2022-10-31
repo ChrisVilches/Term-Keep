7cfb7d52fe9fb098036746a95c86b98cc124773a
Chris Vilches <felovilches@gmail.com>
2022-10-31 23:20:37 +0900

    use connection singleton

--- src/services/db.rs
+++ src/services/db.rs
@@ -3,18 +3,21 @@ use crate::errors::row_not_changed_error::RowNotChangedError;
 use crate::models::traits::FromSqlRow;
 use crate::models::traits::ModelName;
 use crate::util::env;
+use lazy_static::lazy_static;
 use rusqlite::Connection;
+use std::sync::Mutex;
 
 const INSTALL_DATABASE_SQL: &str = include_str!("../../data/install.sql");
 
-pub fn connection() -> rusqlite::Connection {
-  let db_path = env::require_string_env_var("DB_PATH");
-
-  Connection::open(db_path).unwrap_or_else(|e| abort_with_message(e))
+lazy_static! {
+  static ref DB_PATH: String = env::require_string_env_var("DB_PATH");
+  static ref CONNECTION: Mutex<rusqlite::Connection> =
+    Mutex::new(Connection::open(DB_PATH.clone()).unwrap_or_else(|e| abort_with_message(e)));
 }
 
 pub fn install_database() -> Result<(), rusqlite::Error> {
-  connection().execute_batch(INSTALL_DATABASE_SQL)
+  let conn = CONNECTION.lock().unwrap();
+  conn.execute_batch(INSTALL_DATABASE_SQL)
 }
 
 fn row_to_template<T: FromSqlRow>(row: &rusqlite::Row) -> Result<T, rusqlite::Error> {
@@ -22,7 +25,7 @@ fn row_to_template<T: FromSqlRow>(row: &rusqlite::Row) -> Result<T, rusqlite::Er
 }
 
 pub fn rows_to_vec<T: FromSqlRow>(query: &str, params: &[&dyn rusqlite::ToSql]) -> Vec<T> {
-  let conn = connection();
+  let conn = CONNECTION.lock().unwrap();
   let mut stmt = conn.prepare(query).unwrap();
   let mapped = stmt.query_map(params, row_to_template::<T>).unwrap();
   mapped.map(std::result::Result::unwrap).collect()
@@ -39,7 +42,7 @@ pub fn change_row<T: ModelName>(
   query: &str,
   params: &[&dyn rusqlite::ToSql],
 ) -> Result<(), RowNotChangedError> {
-  let conn = connection();
+  let conn = CONNECTION.lock().unwrap();
   let stmt = conn.prepare(query);
   let rows_changed = stmt.unwrap().execute(params).unwrap();
 
@@ -50,7 +53,7 @@ pub fn change_row<T: ModelName>(
 }
 
 pub fn change_rows<T: ModelName>(query: &str, params: &[&dyn rusqlite::ToSql]) -> usize {
-  let conn = connection();
+  let conn = CONNECTION.lock().unwrap();
   let mut stmt = conn.prepare(query).unwrap();
   stmt.execute(params).unwrap()
 }
