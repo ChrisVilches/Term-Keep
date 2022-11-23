72c6a791ddb3f5e3eb9b2ea89a3aef898c151116
Chris Vilches <felovilches@gmail.com>
2022-11-23 12:39:59 +0900

    fix text for result(s) (singular or plural)

--- src/util/search.rs
+++ src/util/search.rs
@@ -1,7 +1,19 @@
 use colored::Colorize;
 
 fn format_search_results_header(query: &str, count: usize) -> String {
-  format!("{} results for {}", count.to_string().bold(), query.bold())
+  format!(
+    "{} {} for {}",
+    count.to_string().bold(),
+    results_string(count),
+    query.bold()
+  )
+}
+
+const fn results_string(count: usize) -> &'static str {
+  match count {
+    1 => "result",
+    _ => "results",
+  }
 }
 
 pub fn format_search_results<T, F>(query: &str, notes: &Vec<T>, format_note: F) -> String
@@ -20,4 +32,19 @@ where
   }
 
   result
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use test_case::test_case;
+
+  #[test_case(0, "results")]
+  #[test_case(1, "result")]
+  #[test_case(2, "results")]
+  #[test_case(3, "results")]
+  #[test_case(4, "results")]
+  fn test_results_string(count: usize, result: &str) {
+    assert_eq!(results_string(count), result);
+  }
 }
