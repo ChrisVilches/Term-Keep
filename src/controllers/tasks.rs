d698b33fe1ec367ab6532d636b3740aacb750341
Chris Vilches <felovilches@gmail.com>
2023-02-13 20:59:10 +0900

    code review

--- src/controllers/tasks.rs
+++ src/controllers/tasks.rs
@@ -6,29 +6,21 @@ use crate::services;
 use crate::util::cli;
 use anyhow::{anyhow, Result};
 
-fn change_aux(
-  task: &Note,
-  current_status: TaskStatus,
-  status: TaskStatus,
-) -> Result<(), RowNotChangedError> {
+fn change_aux(task: &Note, current_status: TaskStatus, status: TaskStatus) {
   if current_status == status {
     println!("{}", cli::color_secondary("Not changed"));
   } else {
-    services::notes::change_task_status(task.require_id(), status as i32)?;
+    services::notes::change_task_status(task.require_id(), status as i32)
+      .expect("Task status should have changed");
   }
-
-  Ok(())
 }
 
 pub fn change_status(task_id: u32, status_str: &str) -> Result<()> {
   let task = services::notes::find_one(task_id)?;
 
   match task.note_type {
-    // TODO: Refactor/simplify this.
     NoteType::Task(current_status) => TaskStatus::from_string(status_str)
-      .map(|new_status| change_aux(&task, current_status, new_status))
-      .map(|_| ())
-      .map_err(|s| anyhow!(s.to_owned())), // TODO: Does this need Err(...) ? Does that work?
+      .map(|new_status| change_aux(&task, current_status, new_status)),
     NoteType::Normal => Err(anyhow!("Not a task")),
   }
 }
